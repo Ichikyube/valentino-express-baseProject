bd5f7ab01f1504231c03abd8cd7dcec59f313df0
Ichikyube <ichikyube@gmail.com>
2022-12-26 21:56:10 +0700

    base-project first commit

---
+++ app/server.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+const responseHelper = require('express-response-helper').helper();
+const express = require ("express")
+const app = express()
+const port = 3000
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const db  = require('./models/index.js')
+
+
+var corsOptions = {
+    origin: "http://localhost:8801"
+}
+app.use(responseHelper)
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+});
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send("this is just a base project")
+})
+
+app.get('/user', async (req, res) => {
+    const user = await db.User.findAll();
+    res.respond(user, 200);
+    // The usual way (without express-response-helper)
+    // res.status(200).json({ name: 'john' });
+  
+    // But with express-response-helper;
+    //res.respond({ name: 'john' });
+  });
+  
+  app.get('/404', function(req, res) {
+    // The usual way (without express-response-helper)
+    // res.status(404).send('Resoure Not Found');
+  
+    // But with express-response-helper;
+    res.failNotFound('Resoure Not Found');
+  
+    // This returns a response like this:
+    /*
+      {
+        "status": 404,
+        "error": "404",
+        "messages": "Resoure Not Found"
+      }
+    */
+  });
+app.listen(port, () => {
+    console.log(`the server run at localhost on port: ${port}`)
+})
